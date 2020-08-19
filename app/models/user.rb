942b7ee4af882d1b33026d5efb54b7b59a22470a
endoakak <akendo2020@gmail.com>
2020-08-19 12:02:32 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?[\d])\w{6,12}\z/.freeze
+  validates :password, format: { with: PASSWORD_REGEX, message: "は6文字以上12文字以下で英数字混合で入力してください", on: :create }
 end
