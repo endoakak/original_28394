9382b67552c69af20894915cf6e7cdae7fb4ab0b
endoakak <akendo2020@gmail.com>
2020-08-19 15:29:31 +0900

    postのバリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,11 @@ class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   end
+
+  with_options presence: true do
+    validates :title
+    validates :content
+    validates :spoiler
+    validates :category
+  end
 end
