a31d7ac3585be3ba6a15588ad78b1ef2d6926ee7
endoakak <akendo2020@gmail.com>
2020-08-25 14:38:18 +0900

    細かい修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :reactions
+  has_many :reactions, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
