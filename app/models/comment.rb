711e779ec744230cd77b890bc9d1de9f1731ad5e
endoakak <akendo2020@gmail.com>
2020-08-27 11:48:28 +0900

    リアクションボタンの修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ActiveHash::Base
   include ActiveHash::Associations
   self.data = [
-    { id: 1, name: "わかります！" },
-    { id: 2, name: "気になります！" },
-    { id: 3, name: "面白い考えですね！" }
+    { id: 1, name: "その気持ち、わかります！" },
+    { id: 2, name: "その作品、気になります！" },
+    { id: 3, name: "その考え、面白いですね！" }
   ]
   has_many :reactions
 end
