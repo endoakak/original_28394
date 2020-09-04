be318bcaafead3ab55d021d111d168e461dc4995
endoakak <akendo2020@gmail.com>
2020-09-04 15:12:43 +0900

    inquirtコントローラーの作成

---
+++ app/controllers/inquiry_controller.rb
@@ -0,0 +1,10 @@
+class InquiryController < ApplicationController
+  def new
+  end
+
+  def confirm
+  end
+
+  def thanks
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'inquiry/new'
+  get 'inquiry/confirm'
+  get 'inquiry/thanks'
   ActiveAdmin.routes(self)
   devise_for :users
   root to: "home#top"
