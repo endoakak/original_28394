dc83ae4e7c14091d1db9bde8649e4b098797b27d
endoakak <akendo2020@gmail.com>
2020-09-04 16:54:15 +0900

    inquiryモデル

---
+++ app/models/inquiry.rb
@@ -0,0 +1,9 @@
+class Inquiry
+  include ActiveModel::Model
+
+  attr_accessor :name, :email, :message
+
+  validates :name, :presence => { :message => 'を入力してください' }
+  validates :email, :presence => { :message => 'を入力してください' }
+  validates :message, :presence => { :message => 'を入力してください' }
+end
