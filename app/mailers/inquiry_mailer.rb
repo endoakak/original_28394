5c2020a4ba00b5434496b8b4fb06bc75f3ada871
endoakak <akendo2020@gmail.com>
2020-09-04 16:47:34 +0900

    mailerの作成

---
+++ app/mailers/inquiry_mailer.rb
@@ -0,0 +1,9 @@
+class InquiryMailer < ApplicationMailer
+  def send_mail(inquiry)
+    @inquiry = inquiry
+    mail(
+      to:   ENV["GMAIL_ADDRESS"],
+      subject: 'Webサイトからお問い合わせがありました'
+    )
+  end
+end
