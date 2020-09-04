23733adccefae93628ec741daae336ae49feabeb
endoakak <akendo2020@gmail.com>
2020-09-04 16:46:45 +0900

    inquiryコントローラー作成

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -1,10 +1,21 @@
 class InquiryController < ApplicationController
   def new
+    @inquiry = Inquiry.new
   end
 
   def confirm
+    @inquiry = Inquiry.new(inquiry_params)
+    render :new unless @inquiry.valid?
   end
 
   def thanks
+    @inquiry = Inquiry.new(inquiry_params)
+    InquiryMailer.send_mail(@inquiry).deliver
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email, :message)
   end
 end
